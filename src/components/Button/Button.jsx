3398c751917adc1c988be07fac14e9f50f8c9ca9
Benjamin Saks <saksben2@gmail.com>
2024-05-06 03:56:32 -0700

    create theme supportive colors and text, add buttons for display

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -22,57 +22,149 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
       >
         {label}
       </button>
-      <div className="bg-main-piccolo">
-        {/* test button to make sure it works */}
-        <button>Button</button>
-      </div>
-      <div className="bg-main-jiren">
-        {/* test button to make sure it works */}
-        <button>Button</button>
-      </div>
-      <div className="bg-main-hit">
-        {/* test button to make sure it works */}
-        <button>Button</button>
-      </div>
-      <div className="bg-main-heles">
-        {/* test button to make sure it works */}
-        <button>Button</button>
-      </div>
-      <div className="bg-main-beerus">
-        {/* test button to make sure it works */}
-        <button>Button</button>
-      </div>
-      <div className="bg-main-goku">
-        {/* test button to make sure it works */}
-        <button>Button</button>
-      </div>
-      <div className="bg-main-gohan">
-        {/* test button to make sure it works */}
-        <button>Button</button>
-      </div>
-      <div className="bg-main-zeno">
-        {/* test button to make sure it works */}
-        <button>Button</button>
-      </div>
-      <div className="bg-main-bulma">
-        {/* test button to make sure it works */}
-        <button>Button</button>
-      </div>
-      <div className="bg-main-trunks">
-        {/* test button to make sure it works */}
-        <button>Button</button>
-      </div>
-      <div className="bg-main-goten">
-        {/* test button to make sure it works */}
-        <button>Button</button>
-      </div>
-      <div className="bg-main-popo">
-        {/* test button to make sure it works */}
-        <button>Button</button>
+      <div style={{ display: "flex" }}>
+        <div>
+          <div className="bg-main-piccolo text-main-piccolo">
+            {/* test button to make sure it works */}
+            <button>Piccolo</button>
+          </div>
+          <div className="bg-main-jiren text-main-jiren">
+            {/* test button to make sure it works */}
+            <button>Jiren</button>
+          </div>
+          <div className="bg-main-hit text-main-hit">
+            {/* test button to make sure it works */}
+            <button>Hit</button>
+          </div>
+          <div className="bg-main-heles text-main-heles">
+            {/* test button to make sure it works */}
+            <button>Heles</button>
+          </div>
+          <div className="bg-main-beerus text-main-beerus">
+            {/* test button to make sure it works */}
+            <button>Beerus</button>
+          </div>
+          <div className="bg-main-goku text-main-goku">
+            {/* test button to make sure it works */}
+            <button>Goku</button>
+          </div>
+          <div className="bg-main-gohan text-main-gohan">
+            {/* test button to make sure it works */}
+            <button>Gohan</button>
+          </div>
+          <div className="bg-main-zeno text-main-zeno">
+            {/* test button to make sure it works */}
+            <button>Zeno</button>
+          </div>
+          <div className="bg-main-bulma text-main-bulma">
+            {/* test button to make sure it works */}
+            <button>Bulma</button>
+          </div>
+          <div className="bg-main-trunks text-main-trunks">
+            {/* test button to make sure it works */}
+            <button>Trunks</button>
+          </div>
+          <div className="bg-main-goten text-main-goten">
+            {/* test button to make sure it works */}
+            <button>Goten</button>
+          </div>
+          <div className="bg-main-popo text-main-popo">
+            {/* test button to make sure it works */}
+            <button>Popo</button>
+          </div>
+        </div>
+        <div>
+          <div className="bg-supportive-krillin-100 text-supportive-krillin-100">
+            <button>Krillin</button>
+          </div>
+          <div className="bg-supportive-chiChi-100 text-supportive-chiChi-100">
+            <button>Chi-Chi</button>
+          </div>
+          <div className="bg-supportive-roshi-100 text-supportive-roshi-100">
+            <button>Roshi</button>
+          </div>
+          <div className="bg-supportive-dodoria-100 text-supportive-dodoria-100">
+            <button>Dodoria</button>
+          </div>
+          <div className="bg-supportive-cell-100 text-supportive-cell-100">
+            <button>Cell</button>
+          </div>
+          <div className="bg-supportive-raditz-100 text-supportive-raditz-100">
+            <button>Raditz</button>
+          </div>
+          <div className="bg-supportive-whis-100 text-supportive-whis-100">
+            <button>Whis</button>
+          </div>
+          <div className="bg-supportive-frieza-100 text-supportive-frieza-100">
+            <button>Frieza</button>
+          </div>
+          <div className="bg-supportive-nappa-100 text-supportive-nappa-100">
+            <button>Nappa</button>
+          </div>
+        </div>
+        <div>
+          <div className="bg-supportive-krillin-60 text-supportive-krillin-60">
+            <button>Krillin-60</button>
+          </div>
+          <div className="bg-supportive-chiChi-60 text-supportive-chiChi-60">
+            <button>Chi-Chi-60</button>
+          </div>
+          <div className="bg-supportive-roshi-60 text-supportive-roshi-60">
+            <button>Roshi-60</button>
+          </div>
+          <div className="bg-supportive-dodoria-60 text-supportive-dodoria-60">
+            <button>Dodoria-60</button>
+          </div>
+          <div className="bg-supportive-cell-60 text-supportive-cell-60">
+            <button>Cell-60</button>
+          </div>
+          <div className="bg-supportive-raditz-60 text-supportive-raditz-60">
+            <button>Raditz-60</button>
+          </div>
+          <div className="bg-supportive-whis-60 text-supportive-whis-60">
+            <button>Whis-60</button>
+          </div>
+          <div className="bg-supportive-frieza-60 text-supportive-frieza-60">
+            <button>Frieza-60</button>
+          </div>
+          <div className="bg-supportive-nappa-60 text-supportive-nappa-60">
+            <button>Nappa-60</button>
+          </div>
+        </div>
+        <div>
+          <div className="bg-supportive-krillin-10 text-supportive-krillin-10">
+            <button>Krillin-10</button>
+          </div>
+          <div className="bg-supportive-chiChi-10 text-supportive-chiChi-10">
+            <button>Chi-Chi-10</button>
+          </div>
+          <div className="bg-supportive-roshi-10 text-supportive-roshi-60">
+            <button>Roshi-10</button>
+          </div>
+          <div className="bg-supportive-dodoria-10 text-supportive-dodoria-60">
+            <button>Dodoria-10</button>
+          </div>
+          <div className="bg-supportive-cell-10 text-supportive-cell-60">
+            <button>Cell-10</button>
+          </div>
+          <div className="bg-supportive-raditz-10 text-supportive-raditz-60">
+            <button>Raditz-10</button>
+          </div>
+          <div className="bg-supportive-whis-10 text-supportive-whis-60">
+            <button>Whis-10</button>
+          </div>
+          <div className="bg-supportive-frieza-10 text-supportive-frieza-60">
+            <button>Frieza-10</button>
+          </div>
+          <div className="bg-supportive-nappa-10 text-supportive-nappa-60">
+            <button>Nappa-10</button>
+          </div>
+        </div>
       </div>
       <div
         style={{
-          width: 0, height: 0,
+          width: 0,
+          height: 0,
         }}
       >
         <Theme>
