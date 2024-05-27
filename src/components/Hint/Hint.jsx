1fe1984e59865ff4054ed1c069d761e0a81972c7
Benjamin Saks <saksben2@gmail.com>
2024-05-26 19:06:49 -0700

    add comments

--- src/components/Hint/Hint.jsx
+++ src/components/Hint/Hint.jsx
@@ -1,6 +1,7 @@
 import { cn } from "../../utils/cn";
 
 export const Hint = ({ disabled, error, className, children }) => {
+  // Styles for Hint
   const stylesDiv = cn(
     "mt-2 flex gap-1 text-trunks text-xs",
     disabled && "opacity-[0.32] cursor-not-allowed",
